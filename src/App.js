e828ecad183b2e7a0bc0ace536c7ea1e2ea49ef6
Adi4512 <adityasharma4510@gmail.com>
2023-12-15 03:20:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Die from "./components/Die.js";
+import {nanoid} from "nanoid";
+import Confetti from 'react-confetti';
 
 function App() {
+//created a state to hold the array of number
+const[dice,setDice]=React.useState(allNewDice); 
+
+//created another state to check whether user is won or not
+
+const[tenzies,setTenzies]=React.useState(false);
+
+//using reactEffect to synchronize both the above states 
+
+React.useEffect(()=>{
+
+     const allHeld=dice.every(die=>die.isHeld)
+      const firstValue=dice[0].value
+      const allSameValue=dice.every(die => die.value===firstValue )
+      if (allHeld && allSameValue) {
+        setTenzies(true)
+        
+      }
+
+
+
+}
+,[dice])
+
+//function to create an array of random numbers
+  function allNewDice(){
+    const newNumberArray=[];
+    for (let i = 0; i < 10; i++) {
+      
+      newNumberArray.push(randomNumberGenerator())
+    }
+    
+    return newNumberArray
+    
+  }
+  //to generate random number 
+ function randomNumberGenerator(){
+  return {
+  
+    value:Math.ceil(Math.random()*6),
+    isHeld:false,
+    id:nanoid()
+  
+  }
+ }
+
+
+  //creating a function to check whether the element is clicked
+
+  function holdDice(id){
+    setDice(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die,isHeld: !die.isHeld}:die
+    }))
+
+  }
+  
+
+
+  //creating function for roll button which onclick generates new array
+  function rollDice(){
+    //adding the feature to restart a the game if game is won
+    if(!tenzies)
+    {
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? 
+      die :
+      randomNumberGenerator()
+    }))
+  }
+  
+  else{
+    setTenzies(false)
+    setDice(allNewDice())
+  }
+  }
+
+  
+  //to create 10 components using map function
+   const diceElements=dice.map(die => <Die holdDice={()=> holdDice(die.id)} isHeld={die.isHeld} key={die.id}  value={die.value}/>)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    {tenzies && <Confetti/>}
+     <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+    <div className='die--container'>
+    {diceElements}
     </div>
+    <button onClick={rollDice} className='roll--button'>{tenzies?"New Game" : "Roll"}</button>
+    </main>
   );
 }
 
